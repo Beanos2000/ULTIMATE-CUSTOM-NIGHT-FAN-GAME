b36255e1bcff96c7f47a97aeadb14d8303f97adc
Beanos2000 <vinnykillia@gmail.com>
2025-12-20 21:04:00 -0500

    idk

--- js/game.js
+++ js/game.js
@@ -1,8 +1,26 @@
+// HTML elements \\
+const menu = document.getElementById("menu");
+const office = document.getElementById("office");
+const startButton = document.getElementById("startBtn");
+const leftDoorButton = document.getElementById("leftDoor");
+const powerDisplay = document.getElementById("power");
+
+// Game variables \\
 let power = 100;
 let leftDoorClosed = false;
+let FredbearHere = false;
 
-document.getElementById("leftDoor").onclick = () => {
-    leftDoorClosed = !leftDoorClosed;
-    console.log(leftDoorClosed ? "Door Closed" : "Door Open");
+// Start Night \\
+startButton.addEventListener('click', () => {
+    menu.style.display = 'none';
+    office.style.display = 'flex';
+    setInterval(gameLoop, 1000);
+});
+
+// Closing the door \\
+leftDoorButton.addEventListener("mousedown", () => leftDoorClosed = !leftDoorClosed);
+
+// Game Loop \\
+function gameLoop() {
     
 }
